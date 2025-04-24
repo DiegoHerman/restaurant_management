8882ed0cbda7fa93332d746ea08c057e27a61670
Wachuguu <mwangipaulduncan@gmail.com>
2025-04-24 06:55:53 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,9 @@
 #include "order.h"  // Include the order header file to access the functions
 
 int main() {
-    // Example inventory for testing
-    InventoryItem inventory[] = {
-        {1, "Pizza", 9.99, 10},
-        {2, "Burger", 5.99, 20},
-        {3, "Pasta", 7.99, 15}
-    };
 
-    int inventorySize = sizeof(inventory) / sizeof(inventory[0]);
 
-    // Display inventory before order
+    int inventorySize = sizeof(inventory) / sizeof(inventory[0])
     printf("Initial Inventory:\n");
     displayInventory(inventory, inventorySize);
 
