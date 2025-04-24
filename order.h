228480a2eaec9cdb1c668aadf0d4c5633301f288
Wachuguu <mwangipaulduncan@gmail.com>
2025-04-24 08:46:30 +0300

    Update order.h
    just added the updated functions to the header file

--- order.h
+++ order.h
@@ -1,37 +1,31 @@
 #ifndef ORDER_H
 #define ORDER_H
 
-typedef struct {
-    int id;
-    char name[50];
-    float price;
-} MenuItem;
 
-typedef struct {
-    int itemID;
+typedef struct 
+{
+    int foodID;
+    char foodName[120];
+    int price;
     int quantity;
-} OrderItem;
 
-// Stack for recent additions
-typedef struct StackNode {
-    OrderItem item;
-    struct StackNode *next;
-} StackNode;
+} FoodOrderItem;
 
-// Queue for submitted orders
-typedef struct OrderNode {
-    OrderItem items[10]; // Up to 10 items per order
-    int itemCount;
-    struct OrderNode *next;
-} OrderNode;
+typedef struct {
+    int customerId;
+    char name[50];
+    FoodOrderItem orders[20];
+    int orderCount;
+    float totalAmount;
+} Customer;
 
-void startOrder(MenuItem menu[], int menuSize);
-void displayMenu(MenuItem menu[], int size);
-void displayOrderQueue();
-void enqueueOrder(OrderItem items[], int count);
-void push(StackNode **top, OrderItem item);
-int pop(StackNode **top, OrderItem *item);
-float calculateTotal(OrderItem orders[], int count, MenuItem menu[], int size);
-void displayCart(OrderItem orders[], int count, MenuItem menu[], int size);
+void FoodOrderMenu();
+void PlaceOrder();
+void PaymentReceipt();
+void getCurrentDate(char *date);
+void displayOrderHistory(int customerIndex);
+void clearInputBuffer();
+int findCustomerbyId(int id);
+
 
 #endif
