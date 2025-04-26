44b883e7833b6bc879a8ce31bb8aabed5c22f55d
Diego <diegovenon456@gmail.com>
2025-04-26 16:30:54 +0300

    Added new admin source and header file

--- admin.c
+++ admin.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "menu.h"
-
-struct addnodequeue {
-    int data;
-    struct addnodequeue *next;
-};
-
-struct fooditem {
-    int foodID;
-    char name[50];
-    float price;
-    int quantity;
-    struct fooditem *next;
-};
+#include <windows.h>
+#include "admin.h"
 
 struct fooditem *head=NULL;
 
 struct fooditem *createfooditem(int foodID,char name[],float price, int quantity) {
-    struct fooditem* newnode=(struct foodname *)malloc(sizeof(struct fooditem));
+    struct fooditem* newnode=(struct fooditem *)malloc(sizeof(struct fooditem));
     if(newnode!=NULL) {
         newnode->foodID=foodID;
     strcpy(newnode->name,name);
@@ -32,14 +20,16 @@ return newnode;
 
 void addfooditem(struct fooditem **head,int foodID,char name[],float price, int quantity) {
     struct fooditem* newnode=createfooditem(foodID,name,price,quantity);
-    struct fooditem *temp=*head;
-    if(temp==NULL) {
+    if(*head==NULL) {
+        *head=newnode;
         return;
     }
+    struct fooditem *temp=*head;
     while(temp->next!=NULL) {
         temp=temp->next;
     }
     temp->next=newnode;
+    printf("Food item added!\n");
 }
 
 void deletefooditem(struct fooditem **head,int foodID) {
@@ -85,6 +75,7 @@ void searchfooditem(struct fooditem *head,int foodID) {
         while(head!=NULL) {
             if (head->foodID==foodID) {
                 printf("%s is avaliable at foodID %d\n",head->name,head->foodID);
+                return;
             }
             head=head->next;
         }
@@ -97,10 +88,10 @@ void displayitems(struct fooditem *head) {
      {
         printf("|----------|----------------------|------------|-----------|\n");
         printf("| %-8s | %-20s | %-10s | %-9s |\n", "FoodID", "Name", "Price (ksh)", "Quantity");
-        printf("|--------|----------------------|------------|-----------|\n");
+        printf("|----------|----------------------|------------|-----------|\n");
     }
    } else {
-    printf("No food items availablen\n");
+    printf("No food items available\n");
     return;
    }
    while(head!=NULL) {
@@ -109,4 +100,90 @@ void displayitems(struct fooditem *head) {
    }
    printf("|----------|----------------------|------------|-----------|\n");
 }
+
+void enqueue(customerqueue *queue,int customerId,char name[],float totalamount) {
+    struct customer *newnode=(struct customer*)malloc(sizeof(struct customer));
+    if(newnode!=NULL) {
+    newnode->customerId=customerId;
+    strcpy(newnode->name,name);
+    newnode->totalamount=totalamount;
+    newnode->next=NULL;
+    }
+    if(queue->front==NULL) {
+        queue->front=queue->rear=newnode;
+    }
+    if(queue->rear!=NULL) {
+        queue->rear->next=newnode;
+        queue->rear=newnode;
+    } 
+}
+
+void loadqueuefromfile(customerqueue *queue,const char *filename) {
+    FILE *file=fopen(filename,"r");
+        if(file==NULL) {
+            printf("File does not exist!\n");
+            return;
+        }
+    char inbuf[300];
+    while(fgets(inbuf,sizeof(inbuf),file)) {
+        char name[50];
+        int customerId;
+        float totalamount;
+
+        if (sscanf(inbuf,"%d,%[^,],%f",&customerId,name,&totalamount)==3) {
+            enqueue(queue,customerId,name,totalamount);
+        }
+    }
+   fclose(file); 
+}
+
+void dequeue(customerqueue *queue) {
+    if(queue->front==NULL) {
+        printf("No order in the queue!\n");
+        return;
+    }
+    customer *temp=queue->front;
+    printf("\n=============================================================\n");
+    printf("Processing CustomerId: %d - Customer: %s - Totalamount: %.2f\n", temp->customerId,temp->name,temp->totalamount);
+    fflush(stdout);
+
+    for (int i = 0; i < 25; ++i)
+    {
+        printf(".");
+        fflush(stdout);
+        Sleep(50);
+    }
+
+    printf("\n");
+    printf("Processed CustomerId: %d -Customer: %s -Totalamount: %.2f\n", temp->customerId, temp->name, temp->totalamount);
+    queue->front=queue->front->next;
+    free(temp);
+}
+
+void displayqueue(customerqueue *queue) {
+    if (queue->front != NULL)
+    {
+        printf("===============================================================================================\n");
+        printf("| %-5s | %-20s | %-30s | %-20s |\n", "S.No.", "Customer ID", "Customer Name", "Total Amount");
+        printf("==============================================================================================\n");
+
+        customer* current = queue->front;
+        int serialNumber = 1;
+        while (current != NULL)
+        {
+            printf("| %-5d | %-20d | %-30s | KSh%-20.2f |\n", serialNumber,current->customerId, current->name, current->totalamount);
+            current = current->next;
+            serialNumber++;
+        }
+
+        printf("===============================================================================================\n");
+    }
+    else
+    {
+        printf("===================================================================================================\n");
+        printf(" No Order queue\n");
+        printf("===================================================================================================\n");
+    }
+}
+
 
--- admin.h
+++ admin.h
@@ -1,17 +1,35 @@
 #ifndef ADMIN_H
 #define ADMIN_H
+ typedef struct customer{
+    int customerId;
+    char name[50];
+    float totalamount;
+    struct customer *next;
+}customer;
+
+typedef struct{
+    customer *front;
+    customer *rear;
+}customerqueue;
 
 struct fooditem {
-    char name[20];
+    int foodID;
+    char name[50];
     float price;
     int quantity;
-
+    struct fooditem *next;
 };
 
 
-void addfooditem();
-void editfooditem();
-void deletefooditem();
-void displayitems();
+void addfooditem(struct fooditem **head,int foodID,char name[],float price, int quantity);
+void deletefooditem(struct fooditem **head,int foodID);
+void editfooditem(struct fooditem **head,int foodID);
+void searchfooditem(struct fooditem *head,int foodID);
+void displayitems(struct fooditem *head);
+
+void enqueue(customerqueue *queue,int customerId,char name[],float totalamount);
+void loadqueuefromfile(customerqueue *queue,const char *filename);
+void dequeue(customerqueue *queue);
+void displayqueue(customerqueue *queue);
 
 #endif
